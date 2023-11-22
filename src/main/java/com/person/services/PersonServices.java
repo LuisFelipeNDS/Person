ca01d286908bf9f332575fd87d418fc0f690fc22
Felipe <Felipesss.2014@gmail.com>
2023-11-22 18:14:50 -0300

    PersonServices e PersonController criados

--- src/main/java/com/person/services/PersonServices.java
+++ src/main/java/com/person/services/PersonServices.java
@@ -1,9 +1,31 @@
 package com.person.services;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
 
+import org.springframework.stereotype.Service;
+
+import com.person.model.Person;
+
+@Service
 public class PersonServices {
 	
 	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	public Person findById(String id) {
+		
+		logger.info("Findind one person");
+		Person person = new Person();
+		
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Luis");
+		person.setLastName("Neves");
+		person.setAdress("Orindiuva");
+		person.setGender("Male");
+		
+		return person;
+	}
+	
 
 }
