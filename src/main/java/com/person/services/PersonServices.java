127d199a1dcb8a965d58f50c9c219956a7a5926e
Felipe <Felipesss.2014@gmail.com>
2023-11-22 18:30:20 -0300

    metodo findAll criado

--- src/main/java/com/person/services/PersonServices.java
+++ src/main/java/com/person/services/PersonServices.java
@@ -1,5 +1,7 @@
 package com.person.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -9,23 +11,48 @@ import com.person.model.Person;
 
 @Service
 public class PersonServices {
-	
+
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
-	
+
+	public List<Person> findAll() {
+
+		logger.info("Findind all people");	
+		List<Person> persons = new ArrayList<>();
+
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+
+		return persons;
+	}
+
 	public Person findById(String id) {
-		
+
 		logger.info("Findind one person");
 		Person person = new Person();
-		
+
 		person.setId(counter.incrementAndGet());
 		person.setFirstName("Luis");
 		person.setLastName("Neves");
 		person.setAdress("Orindiuva");
 		person.setGender("Male");
-		
+
 		return person;
 	}
-	
+
+	private Person mockPerson(int i) {
+		
+		Person person = new Person();
+
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("lastName " + i);
+		person.setAdress("Some adress in Brasil " + i);
+		person.setGender("Male");
+
+		return person;
+	}
 
 }
